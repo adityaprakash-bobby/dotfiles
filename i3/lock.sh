f38f27eb020c4d94ecfa26cfbbf20fc1c181ad30
Aditya Prakash <adityaprakash.bobby@gmail.com>
2018-06-19 18:27:39 +0530

    Update lock.sh

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# i3lock blurred screen inspired by /u/patopop007 and the blog post
-# http://plankenau.com/blog/post-10/gaussianlock
-
-# Timings are on an Intel i7-2630QM @ 2.00GHz
-
-# Dependencies:
-# imagemagick
-# i3lock
-# scrot (optional but default)
-
 IMAGE=/tmp/i3lock.png
 SCREENSHOT="scrot $IMAGE" # 0.46s
 
