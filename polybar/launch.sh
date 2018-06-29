c616cd880e2001d84b6046c1696f260be13a4089
Aditya Prakash <aditya@localhost.localdomain>
2018-06-29 23:37:57 +0530

    Updated bars

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,8 +4,11 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar PolybarTony &
+# Launch upperbar
+polybar  PolybarTony &
+
+
+echo "Bar launched..."
 
