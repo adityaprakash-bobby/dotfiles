79823741e522f82ca308d5574e15f1607d3c3a1d
adityaprakash-bobby <adityaprakash.bobby@gmail.com>
2018-08-02 20:20:14 +0530

    bottom bar added

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,8 +10,11 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 if xrandr | grep "HDMI-1 connected"; then
 	MONITOR=HDMI-1 polybar PolybarTony &
 	MONITOR=eDP-1 polybar PolybarTony &
+	MONITOR=HDMI-1 polybar bottom &
+	MONITOR=eDP-1 polybar bottom &
 else
 	MONITOR=eDP-1 polybar PolybarTony &
+	MONITOR=eDP-1 polybar bottom &
 fi
 
 echo "Bar launched..."
